357ecdb7b1f9cfca47757fdd04c2ac33f2395637
Jim Haff <Jimmyhaff3@gmail.com>
2018-04-04 15:29:40 -0500

    thunks complete

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import counter from './counter';
+
+console.log(counter)
 
 export default combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  counter
 })
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+
+// Redux thunk is middleware for Redux that allows you to
+// write action creators that return a function instead of an action
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './modules';
