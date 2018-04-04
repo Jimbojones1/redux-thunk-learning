11008a2f18bac1dabffb40cf679d11d6461b1b19
Jim Haff <Jimmyhaff3@gmail.com>
2018-04-04 12:19:16 -0500

    added home component

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -7,20 +7,12 @@ const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
-      <Link to="/about-us">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
 
-
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
   </div>
-
-
-
-
-
-
-
-
-
-
-
-
 )
